dd6458c43bc186a50bb65e98de54006b66bebd51
A616\1DAW <alvaroperezllorens20@gmail.com>
2025-10-16 16:01:05 +0200

    multiplicaciones final

--- Ejercicios_Programacion/src/main/java/org/example/Multiplicaciones.java
+++ Ejercicios_Programacion/src/main/java/org/example/Multiplicaciones.java
@@ -14,12 +14,12 @@ public class Multiplicaciones {
         boolean repetir = false;
         do {
             try{
-                System.out.println("Introduce el multiplicando(3 cifras): ");
+                System.out.println("Introduce el multiplicando(3 cifras): ");//pido numeros
                 num1 = teclado.nextInt();
-                System.out.println("Introduce el multiplicador(3 cifras): ");
+                System.out.println("Introduce el multiplicador(3 cifras): ");//pido numeros
                 num = teclado.nextInt();
 
-                if (((num1<100 || num1>999) && (num1>-100 || num1<-999) || (num<100 || num>999) && (num>-100 || num<-999))){
+                if (((num1<100 || num1>999) && (num1>-100 || num1<-999) || (num<100 || num>999) && (num>-100 || num<-999))){//si el numero tiene 1,2,4 o mas cifras da error, incluyendo negativos y se repite
                     System.out.println("ERROR: solo se permiten numeros de 3 cifras");
                     repetir = false;
                 }else {
@@ -31,39 +31,39 @@ public class Multiplicaciones {
                 repetir = false;
             }
         }while (repetir==false);
-        String num_string = Integer.toString(num);
-        if (num<0){
+        String num_string = Integer.toString(num); //paso num a string
+        if (num<0){ //si num es negativo
 
-            String num_izquierda = num_string.substring(1,2);
-            num_izquierda_int = Integer.parseInt(num_izquierda);
-            String num_centro = num_string.substring(2,3);
-            num_centro_int = Integer.parseInt(num_centro);
-            String num_derecha = num_string.substring(3,4);
-            num_derecha_int = Integer.parseInt(num_derecha);
+            String num_izquierda = num_string.substring(1,2); //coje el segundo caracter ya que el primero es un "-"
+            num_izquierda_int = Integer.parseInt(num_izquierda);//lo guardo
+            String num_centro = num_string.substring(2,3);//cojo el tercer caracter
+            num_centro_int = Integer.parseInt(num_centro);//lo guardo
+            String num_derecha = num_string.substring(3,4);//cojo el cuarto caracter
+            num_derecha_int = Integer.parseInt(num_derecha);//lo guardo
 
-        }else{
+        }else{ //si es positivo
 
-            String num_izquierda = num_string.substring(0,1);
-            num_izquierda_int = Integer.parseInt(num_izquierda);
-            String num_centro = num_string.substring(1,2);
-            num_centro_int = Integer.parseInt(num_centro);
-            String num_derecha = num_string.substring(2,3);
-            num_derecha_int = Integer.parseInt(num_derecha);
+            String num_izquierda = num_string.substring(0,1);//cojo el primer caracter
+            num_izquierda_int = Integer.parseInt(num_izquierda);//lo guardo
+            String num_centro = num_string.substring(1,2);//cojo el segundo caracter
+            num_centro_int = Integer.parseInt(num_centro);//lo guardo
+            String num_derecha = num_string.substring(2,3);//cojo el tercer caracter
+            num_derecha_int = Integer.parseInt(num_derecha);//lo guardo
 
         }
-        int res = num1*num_derecha_int;
-        int res2 = num1*num_centro_int;
-        int res3 = num1*num_izquierda_int;
-        System.out.println("El producto de la multiplicación es: "+num1*num);
+        int res = num1*num_derecha_int; //multiplico los numeros guardados por el multiplicando
+        int res2 = num1*num_centro_int;//multiplico los numeros guardados por el multiplicando
+        int res3 = num1*num_izquierda_int;//multiplico los numeros guardados por el multiplicando
+        System.out.println("El producto de la multiplicación es: "+num1*num);//muestor el resultado de la multiplicacion
         System.out.println("El proceso es: ");
-        System.out.println("  "+num1);
-        System.out.println("x "+num);
+        System.out.println("  "+num1);//muestro multiplicando
+        System.out.println("x "+num);//muestro multiplicador
         System.out.println("------");
-        System.out.println("  "+res);
-        System.out.println(" "+res2+"x");
-        System.out.println(res3+"xx");
+        System.out.println("  "+res);//muestros los resultados separados
+        System.out.println(" "+res2+"x");//muestros los resultados separados
+        System.out.println(res3+"xx");//muestros los resultados separados
         System.out.println("------");
-        System.out.println(num1*num);
+        System.out.println(num1*num);//muestro el resultado final
 
     }
 }
---
+++ Ejercicios_Programacion/src/main/java/org/example/ejer3t2.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class ejer3t2 {
+}
