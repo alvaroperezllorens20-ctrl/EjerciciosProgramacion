32b9cb901bec2cb5334ab5aac2fa99d7dbaac57c
A616\1DAW <alvaroperezllorens20@gmail.com>
2025-10-27 21:16:59 +0100

    RANDOM

---
+++ Ejercicios_Programacion/src/main/java/org/example/random.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Random;
+
+public class random {
+    static void main() {
+        Random aleatorio = new Random();
+//EJER 1
+//       // int numero = aelatorio.nextInt(2)+1;
+//       // System.out.println(numero);
+//        int num = aleatorio.nextInt(6)+1;
+//        System.out.println(num);
+//
+//        int num2 = aleatorio.nextInt(6)+1;
+//        System.out.println(num2);
+//
+//        int suma = num + num2;
+//        System.out.println("La suma de los numero es: "+suma);
+// EJER 2
+//        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//        String contra = "";
+//        for (int i = 0; i<12;i++){
+//            contra += caracteres.charAt(aleatorio.nextInt(caracteres.length()-1));
+//        }
+//        System.out.println(contra);
+        // EJER 3
+    }
+}
