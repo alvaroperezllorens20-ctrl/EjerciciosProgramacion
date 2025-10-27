0d6008f674d432f562740c1a29e57be7ae55c1bb
A616\1DAW <alvaroperezllorens20@gmail.com>
2025-10-27 21:12:57 +0100

    isbn final

--- Ejercicios_Programacion/src/main/java/org/example/ISBN.java
+++ Ejercicios_Programacion/src/main/java/org/example/ISBN.java
@@ -15,6 +15,7 @@ public class ISBN {
         int tamanyo = 0;
         int falta = 0;
         String num = "0";
+        int numero = 0;
         do {
             try {
                 System.out.println("VALIDADOR DE ISBN");
@@ -36,6 +37,8 @@ public class ISBN {
                 repetir = true;
             }catch (Exception err){
                 System.out.println("ERROR: Formato incorrecto.");
+                repetir = false;
+                teclado.nextLine();
             }
         }while (repetir==false);
         switch (opcion){
@@ -45,21 +48,20 @@ public class ISBN {
                 ISBN = ISBN.toUpperCase();
                 tamanyo = ISBN.length();
                 if (tamanyo != 10) {
-                    System.out.println("Foramto incorrecto. Introduce 10 caractéres.");
+                    System.out.println("Formato incorrecto. Introduce 10 caractéres.");
                 }
                 for (int i = 0; i < tamanyo; i++) {
-                    num = ISBN.substring(i);   // cojo todo el isbn
+                    num = ISBN.substring(i);
                     num = num.substring(0,1);
 
 
-                    if (num.equals("X")) {
+                    if (i == 9 && num.equals("X")) {
                         num = "10";
                         num_int = Integer.parseInt(num);
                     }
-                    // Si el carácter es un número entre '0' y '9'
-                    num_int = Integer.parseInt(num); // Convierte el carácter a número
-                    total += num_int * cont; // Suma ponderada
-                    cont--; // Decrementa el peso (10 → 1)
+                    num_int = Integer.parseInt(num);
+                    total += num_int * cont;
+                    cont--;
                 }
                 valido = total % 11;
                 if (valido == 0){
@@ -69,36 +71,38 @@ public class ISBN {
                 }
                 break;
             case 2:
-                System.out.println("introduce un ISBN (máximo 10 dígitos): ");
+                System.out.println("Introduce un ISBN (máximo 10 dígitos): ");
                 ISBN = teclado.nextLine();
                 ISBN = ISBN.toUpperCase();
                 tamanyo = ISBN.length();
+
                 if (tamanyo != 10) {
-                    System.out.println("Foramto incorrecto. Introduce 10 caractéres.");
+                    System.out.println("Formato incorrecto. Introduce 10 caractéres.");
                 }
                 for (int i = 0; i < tamanyo; i++) {
-                    num = ISBN.substring(i);   // cojo todo el isbn
+                    num = ISBN.substring(i);
                     num = num.substring(0,1);
 
-
                     if (num.equals("?")) {
-                        falta = cont; // guardamos el peso del signo
-                    } else {
-                        if (num.equals("X")){
-                            num = "10";
-                            num_int = Integer.parseInt(num);
-                        }
+                        falta = cont;
+                        cont--;
+                        continue;
                     }
-                    num_int = Integer.parseInt(num); // Convierte el carácter a número
-                    total += num_int * cont; // Suma ponderada
-                    cont--; // Decrementa el peso (10 → 1)
+
+                    if (i == 9 && num.equals("X")) {
+                        num = "10";
+                    }
+                    num_int = Integer.parseInt(num);
+                    total += num_int * cont;
+                    cont--;
                 }
-                valido = total % 11;
-                if (valido == 0){
-                    System.out.println("El ISBN es válido");
-                }else{
-                    System.out.println("El ISBN no es válido");
-                }
+                int pos = 0;
+                do {
+                    pos++;
+                    numero = falta * pos;
+                    total = total + numero;
+                }while (total % 11 != 0);
+                System.out.println("El digito que falta es: "+pos);
                 break;
             case 3:
                 System.out.println("Saliendo...");
