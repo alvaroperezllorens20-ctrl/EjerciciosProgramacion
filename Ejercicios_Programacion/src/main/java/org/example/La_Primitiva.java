3c3381b78856aed3596da00a8995e4ee058160d3
A616\1DAW <alvaroperezllorens20@gmail.com>
2025-11-17 20:45:00 +0100

    primitiva

--- Ejercicios_Programacion/src/main/java/org/example/La_Primitiva.java
+++ Ejercicios_Programacion/src/main/java/org/example/La_Primitiva.java
@@ -8,43 +8,51 @@ public class La_Primitiva {
     static void main() {
         Random aleatorio = new Random();
         Scanner teclado = new Scanner(System.in);
-        int numero = aleatorio.nextInt(1,50);
+        int boleto_ganador[] = new int[8];
         String complementario = "";
         String reintegro = "";
         boolean repetir = true;
-
+        String[] vector = {};
         String boleto = "";
 
         do {
             System.out.println("Introduce tu boleto: ");
             boleto = teclado.nextLine();
-
-            repetir = boleto.matches("\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}");
-            complementario = boleto.substring(18,20);
-            reintegro = boleto.substring(21,23);
-        }while(!repetir);
-            System.out.println(boleto);
-            System.out.println(complementario);
-            System.out.println(reintegro);
-            String[] vector = {};
-
-            for (int i = 0;i < boleto.length();i++){
-
-                vector = boleto.split("-");
+            if (boleto.length() <= 23) {
+                repetir = boleto.matches("\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}");
+                complementario = boleto.substring(18, 20);
+                reintegro = boleto.substring(21, 23);
+                break;
+            } else {
+                continue;
             }
-        String vector_in = "";
-            System.out.println(Arrays.toString(vector));
-        while(repetir){
-            repetir = false;
-            for (int i = 0;i < vector.length;i++){
-                int vector_int = Integer.parseInt(vector[i]);
-                if (i != vector.length-1 && vector[i] == vector[i+1]) {
-                    vector_int = aleatorio.nextInt(50);
+        } while (repetir);
 
-                    repetir = true;
-                }
+        //boleto participante
+        vector = boleto.split("-");
+
+        //creo boleto ganador
+        for (int i = 0; i < boleto_ganador.length; i++) {
+
+            if (i == 7) {
+                boleto_ganador[i] = aleatorio.nextInt(0, 10);
+            } else {
+                boleto_ganador[i] = aleatorio.nextInt(1, 50);
             }
         }
-       // System.out.println(Arrays.toString(vector_int));
+
+        for (int i = 0; i < boleto_ganador.length; i++) {
+            int vector_int = Integer.parseInt(vector[i]);
+
+            if (vector_int == boleto_ganador[i]) {
+                System.out.println("Enhorabuena. Has acertados todos los numeros");
+                break;
+            }
+            System.out.println(Arrays.toString(boleto_ganador));
+
+            System.out.println(Arrays.toString(vector));
+            System.out.println("Complementario: " + complementario);
+            System.out.println("Reintegro: " + reintegro);
+        }
     }
 }
